90a7b1338e157365384f57c78ee2b0d0cf3d9a9d
Brette Fitzgibbon <79230918+BretteConnolly@users.noreply.github.com>
2025-01-28 23:14:40 -0800

    Added test for RandR

--- submission.py
+++ submission.py
@@ -1,23 +1,30 @@
-import MonteCarlo
+#import MonteCarlo
 import ZeroR
-import perceptron
+# import perceptron
 
-print("testing Monte Carlo")
-MonteCarlo.monte_carlo_approach(10)
+# print("testing Monte Carlo")
+# MonteCarlo.monte_carlo_approach(10)
 
 print("testing ZeroR")
 fname = "tennis.csv"
-with open(fname) as f :
+with open(fname) as f:
     data = f.readlines()
     print(ZeroR.zeroR(data))
 print("You can test the command line arguments from the command line.")
 
-print("Testing wc")
-print("You can run wc from the command line, like so")
-print("wc --strip ~/Documents   or")
-print("wc --strip --pfile=works.pkl ~/Documents")
-
-print("testing perceptron")
-perceptron.perceptron_training()
-
-print("")
+print("testing RandR")
+fname = "tennis.csv"
+with open(fname) as f:
+    data = f.readlines()
+    print(ZeroR.randR(data))
+print("You can test the command line arguments from the command line.")
+#
+# print("Testing wc")
+# print("You can run wc from the command line, like so")
+# print("wc --strip ~/Documents   or")
+# print("wc --strip --pfile=works.pkl ~/Documents")
+#
+# print("testing perceptron")
+# perceptron.perceptron_training()
+#
+# print("")
